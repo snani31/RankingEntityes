5cdd531015e6f47a19f8f762e442a4c55fae7a67
Artem Pop <vip.artem.popov.14@mail.ru>
2025-02-14 19:53:40 +0500

    MediaObject class add

---
+++ Ranking_Entityes/MediaObjacts/MediaObject.cs
@@ -0,0 +1,31 @@
+﻿using RankingEntityes.IO_Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankingEntityes.Ranking_Entityes.MediaObjacts
+{
+    public class MediaObject : IO_Entities.Interfaces.ISerializable, IDeserializable
+    {
+        public Guid ID { get; set; }
+        public string Description {  get; set; } = String.Empty;
+        public string Tytle { get; set; } = String.Empty;
+        public List<string> Paths { get; set; }
+        public MediaObject()
+        {
+            Paths = new List<string>();
+            Paths.Add(@"\AppResources\PowerOffButtonIcon.png");
+        }
+        public bool Deserialize(IDeserializer deserializer, string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Serialize(ISerializer Serializer, string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
