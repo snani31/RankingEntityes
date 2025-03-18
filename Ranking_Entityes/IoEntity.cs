0024bd127188faaf344702b67e592bf269f5f393
Artem Pop <vip.artem.popov.14@mail.ru>
2025-03-18 09:12:47 +0500

    add new IoEntity abstract class

---
+++ Ranking_Entityes/IoEntity.cs
@@ -0,0 +1,18 @@
+﻿using RankingEntityes.IO_Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankingEntityes.Ranking_Entityes
+{
+    public abstract class IoEntity : ISerializable, IDeserializable
+    {
+        public Guid ID { get; set; }
+
+        public abstract bool Deserialize(IDeserializer deserializer, string path);
+
+        public abstract bool Serialize(ISerializer Serializer, string path, FileMode mode);
+    }
+}
