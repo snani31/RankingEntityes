a4900b5df4d54b729d3cc52c875f1362f04dd0aa
Artem Pop <vip.artem.popov.14@mail.ru>
2025-05-09 13:30:36 +0500

    IHasSearchKeyWords interface data model creating

---
+++ Ranking_Entityes/IHasSearchedKeyWords.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RankingEntityes.Ranking_Entityes
+{
+    public interface IHasSearchedKeyWords
+    {
+        public List<string> GetSearchDataWords();
+    }
+}
